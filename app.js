810364915258e2d939668fe0432bf97d02669056
ma-lucifer <68775519+ma-lucifer@users.noreply.github.com>
2021-08-05 18:27:09 +0530

    i change value of input to nummbers that means we can only put number in input area

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function find(){
             res = (x-32)*5/9;
             document.querySelector('.ShowArea').innerHTML = "The Celsius value of "+ x +" F is "+ res +" <sup>o</sup>C";
         }
-    document.querySelector('.tq').innerText = "Thank You For Using";
+        document.querySelector('.tq').innerText = "Thank You For Using";
 
     }else{
         document.querySelector('.inpt').style.border = "1px solid red";
